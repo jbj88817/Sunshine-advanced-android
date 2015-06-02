2fca01ba5ea4d075054c8ec477f09a84871fcb86
Bojie Jiang <jiangbojie@gmail.com>
2015-06-01 21:46:08 -0700

    override the showDialog and get the edittext make the Ok button enable in diff situation

--- app/src/main/java/com/bojie/sunshine_advanced_android/LocationEditTextPreference.java
+++ app/src/main/java/com/bojie/sunshine_advanced_android/LocationEditTextPreference.java
@@ -1,9 +1,16 @@
 package com.bojie.sunshine_advanced_android;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.os.Bundle;
 import android.preference.EditTextPreference;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.AttributeSet;
+import android.widget.Button;
+import android.widget.EditText;
 
 /**
  * Created by bojiejiang on 5/30/15.
@@ -12,6 +19,7 @@ public class LocationEditTextPreference extends EditTextPreference {
 
     private int mMinLength;
     private static final int DEFAULT_MINIMUM_LOCATION_LENGTH = 2;
+    EditText mEditTextDialog;
 
     public LocationEditTextPreference(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -24,5 +32,35 @@ public class LocationEditTextPreference extends EditTextPreference {
         } finally {
             typedArray.recycle();
         }
+    }
+
+    @Override
+    protected void showDialog(Bundle state) {
+        super.showDialog(state);
+        mEditTextDialog = getEditText();
+        mEditTextDialog.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                Dialog dialog = getDialog();
+                if (dialog instanceof AlertDialog) {
+                    Button positiveButton = ((AlertDialog) dialog).getButton(AlertDialog.BUTTON_POSITIVE);
+                    if (s.length() < mMinLength) {
+                        positiveButton.setEnabled(false);
+                    } else {
+                        positiveButton.setEnabled(true);
+                    }
+                }
+            }
+        });
     }
 }
